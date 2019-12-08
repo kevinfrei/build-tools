1b1dd13a5fbd678a3125677d908ec7155377a614
Kevin Frei <kevinfrei@hotmail.com>
2019-12-08 12:36:35 -0800

    Stuff as I have it in my private blob...

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # build-tools
 My nodejs build tools (mostly for flow &amp; babel)
+
+See it's use in my other repo's, such as media-utils
---
+++ babelify.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+// @format
+
+// This is the executable entry point to use:
+require('./index').Babel(...process.argv.slice(2));
