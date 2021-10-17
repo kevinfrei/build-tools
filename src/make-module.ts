e2e5d6aed48dc96a40b495fad43976c155065fa3
Kevin Frei <kevinfrei@hotmail.com>
2021-10-17 16:54:02 -0700

    v3.1.2

--- src/make-module.ts
+++ src/make-module.ts
@@ -2,33 +2,31 @@
 // https://adostes.medium.com/authoring-a-javascript-library-that-works-everywhere-using-rollup-f1b4b527b2a9
 
 import nodeResolve from '@rollup/plugin-node-resolve';
-import { rollup, RollupOptions } from 'rollup';
-
-const err = console.error; // eslint-disable-line no-console
-const log = console.log; // eslint-disable-line no-console
+import { OutputOptions, rollup, RollupOptions } from 'rollup';
+import dts from 'rollup-plugin-dts';
 
 const inputOptions: RollupOptions = {
-  plugins: [nodeResolve()],
+  plugins: [nodeResolve(), dts()],
 };
-const outputOptions: RollupOptions = {
-  output: [
-    { dir: 'dist/esm', format: 'esm', exports: 'named', sourcemap: 'inline' },
-    { dir: 'dist/cjs', format: 'cjs', exports: 'named', sourcemap: 'inline' },
-  ],
+const esmOptions: OutputOptions = {
+  dir: 'dist/esm',
+  format: 'esm',
+  exports: 'named',
+  sourcemap: true,
+};
+const cjsOptions: OutputOptions = {
+  dir: 'dist/cjs',
+  format: 'cjs',
+  exports: 'named',
+  sourcemap: true,
 };
 
 export async function makeDualModeModule(root: string[]): Promise<number> {
   const bundle = await rollup({ input: root, ...inputOptions });
-  log(bundle.watchFiles);
-  const { output } = await bundle.generate(outputOptions);
-  for (const chunkOrAsset of output) {
-    if (chunkOrAsset.type === 'asset') {
-      log('Asset:', chunkOrAsset);
-    } else {
-      log('Chunk:', chunkOrAsset.modules);
-    }
-  }
-  await bundle.write(outputOptions);
+  await bundle.generate(esmOptions);
+  await bundle.write(esmOptions);
+  await bundle.generate(cjsOptions);
+  await bundle.write(cjsOptions);
   await bundle.close();
   return 0;
 }
